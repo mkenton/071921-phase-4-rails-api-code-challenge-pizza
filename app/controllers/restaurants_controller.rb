93ec429b860e36a84ca55905d46b6453698ae561
Michael <mkenton2020@icloud.com>
2021-09-28 11:50:08 -0500

    added index to restaurants and pizza, added show to restaurants with exception error

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,20 @@
 class RestaurantsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :restaurant_not_found
+    #GET /restaurants
+    def index
+        restaurants = Restaurant.all
+        render json: restaurants
+    end
+
+    # GET /restaurants/:id
+    def show
+        restaurant = Restaurant.find(params[:id])
+        render json: restaurant, include: :pizzas
+    end
+
+    private
+    def restaurant_not_found
+        render json: { error: "Restaurant not found" }, status: :not_found
+    end
+
 end
